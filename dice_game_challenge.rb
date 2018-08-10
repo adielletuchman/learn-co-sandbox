a759cd5d28db5fe166313c437fdc93d21bea3b79
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-10 14:46:26 +0000

    automatically backed up by learn

---
+++ dice_game_challenge.rb
@@ -0,0 +1,53 @@
+dice_rolls = [1, 2, 3, 4, 5, 6]
+adielle_rolls = []
+
+counter = 0 
+while counter < 5 
+  adielle_rolls.push(dice_rolls.sample)
+counter += 1 
+
+end 
+
+puts adielle_rolls
+
+
+my_total= 0 
+adielle_rolls.each do |roll|
+  my_total = my_total + roll 
+
+end 
+puts my_total
+
+
+
+
+
+
+
+
+
+
+
+
+# computer_rolls = [1, 2, 4, 6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
+
+#STEP 1
+# Simulate the rolling of a die 5 times
+
+# STEP 2
+#Push each roll to an array
+
+# STEP 3
+#Print out the array
+
+# STEP 4
+# Print out the total of your 5 rolls
+
+# STEP 5
+# Compare your total to the computer’s total (a random number chosen from the computer_array)
+
+# STEP 6
+# Determine the winner/if it is a tie and output a message that congratulates the winner! 
+
+
+
